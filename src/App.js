672a122a70471f88ecba0f164bee011bc3bfe732
arif99050 <arifulislam99050@gmail.com>
2022-04-14 14:57:11 +0600

    work with error 404 route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Pages/About/About';
 import Home from './Pages/Home/Home/Home';
+import NotFound from './Pages/NotFound/NotFound';
 import Header from './Pages/Shared/Header/Header';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/about' element={<About></About>}></Route>
+        <Route path='*'element={<NotFound></NotFound>}></Route>
       </Routes>
      
     </div>
