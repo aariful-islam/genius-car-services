b882e664536bb7ac526dc750904cca22174c59e9
arif99050 <arifulislam99050@gmail.com>
2022-04-14 12:21:08 +0600

    work with experts section, show experts in home

--- src/Pages/Home/Expert/Expert.js
+++ src/Pages/Home/Expert/Expert.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
+import { Card, Col } from "react-bootstrap";
 
-const Expert = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const Expert = ({ expert }) => {
+  const { name, img } = expert;
+  return (
+    
+      <Col lg={4} sm={12} md={6}>
+        <Card className="border shadow-lg p-3 mb-5 bg-white rounded">
+          <Card.Img variant="top" src={img} />
+          <Card.Body>
+            <Card.Title>{name}</Card.Title>
+
+            <button className="bg-primary text-white">Go somewhere</button>
+          </Card.Body>
+        </Card>
+      </Col>
+    
+  );
 };
 
 export default Expert;
